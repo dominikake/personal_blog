cec2598d23e28d39ceee11ff145a200cd499e0a3
Jan Go <janemgo86@gmail.com>
2023-09-18 20:51:07 +0800

    updated title

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Create Next App</title>
+        <title>Jan Go Site</title>
         <meta
           name="description"
           content="Thoughts, Discoveries, Tech Unveilede"
