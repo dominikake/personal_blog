dbec99be3f37f3c6007092645e77db28d04b0726
Jan Go <janemgo86@gmail.com>
2025-01-27 20:05:05 +0800

    fix cursor animation timing

--- src/components/BlinkingCursor.tsx
+++ src/components/BlinkingCursor.tsx
@@ -4,16 +4,11 @@ import { useState, useEffect } from "react"
 import styles from "./BlinkingCursor.module.css"
 
 export default function BlinkingCursor() {
-  const [visible, setVisible] = useState(true)
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setVisible((v) => !v)
-    }, 530)
-
-    return () => clearInterval(interval)
-  }, [])
-
-  return <span className={styles.cursor}>{visible ? "_" : " "}</span>
+  
+  return (
+  <span className={styles.cursor}>
+  _
+  </span>
+  )
 }
 
